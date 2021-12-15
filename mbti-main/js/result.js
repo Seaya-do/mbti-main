ad2b6383ec76f34d77a22ec291de370d8e898f84
Seaya-do <67862904+Seaya-do@users.noreply.github.com>
2021-12-15 22:32:32 +0900

    .

--- mbti-main/js/result.js
+++ mbti-main/js/result.js
@@ -1,4 +1,5 @@
-const results = document.querySelector(".re");
+const result = document.querySelector("#result");
+const qs = document.querySelector("#qs");
 
 const rIndex = 0; //qIndexd에서 받은 질문을 넣을 변수
 const endPoint = 80;
@@ -6,14 +7,37 @@ const endSelect = [];
 
 
 
+function calResult(list, types){
+    for (var i = 0, len = list.length; i < len; i++) {
+        if (list[i].types == types) return list[i];
 
+    }
+}
+console.log(calResult(resultsList,'INFP'));
+r.innerHTML = calResult(resultsList, 'INFP').desc;
+    
+
+
+function setResult(){
+    let endpoint = calResult();
+    const resultName = document.querySelector('.resultname');
+    resultName.innerHTML = resultsList[endpoint].types;
+
+
+    const resultDesc = document.querySelector('.resultDesc');
+    resultDesc.innerHTML = resultsList[endpoint].desc;
+}
 
 
 function goResult(){
-    let results = resultsList.find(v => v.types ==='INTP');
-    console.log(results);
-
-    document.getElementById("result").style.display = "block"; // result 섹션 보이게
-    document.getElementById("test").style.display = "none"; // test 섹션 보이게
+    qs.style.WebkitAnimation = "fadeOut 1s";
+    qs.style.animation = "fadeOut 1s";
+    setTimeout(() => {
+        result.style.WebkitAnimation = "fadeIn 1s";
+        result.style.animation = "fadeIn 1s";
+        setTimeout(() => {
+            qna.style.display = "none";
+            result.style.display = "block"
+        }, 450)})
+    setResult();
 }
-
